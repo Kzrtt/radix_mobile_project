35c111d72d0a439d76efc8f6a02826e9012dfaa8
Kurt <fe.hatunaqueton@gmail.com>
2022-06-14 16:59:55 -0300

    mudanças no provider do carrinho e na página do carrinho, criação do provider do vendedor

--- lib/providers/cartProvider.dart
+++ lib/providers/cartProvider.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:radix_mobile_project/model/endereco.dart';
 import 'package:radix_mobile_project/model/item.dart';
+import 'package:radix_mobile_project/providers/adressProvider.dart';
 
 class CartProvider with ChangeNotifier {
   List<Item> _items = [];
   int _quantity = 1;
+  List<Endereco> _enderecos = [];
 
+  //getters
   List<Item> get items => [..._items];
   int get quantity => _quantity;
+
+  //Métodos para criar um endereço principal
+  /*
+  void toggleEnderecoPrincipal(List<Endereco> _e, Endereco _e1) {
+    if (_e.any((element) => element.isEnderecoPrincipal == true)) {
+      Endereco temp = _e.where((element) => element.isEnderecoPrincipal == true)
+          as Endereco;
+      temp.isEnderecoPrincipal == false;
+      _e1.isEnderecoPrincipal == true;
+      notifyListeners();
+    } else {
+      _e1.isEnderecoPrincipal == true;
+      notifyListeners();
+    }
+  }
+
+  Endereco getEnderecoPrincipal(List<Endereco> enderecos) {
+    return enderecos
+        .singleWhere((element) => element.isEnderecoPrincipal == true);
+  }
+
+  bool isPrimaryAdress(Endereco e) {
+    return e.isEnderecoPrincipal;
+  }
+  */
 
   //Métodos referentes ao carrinho
   void inc() {
